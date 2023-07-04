bb1fcaa368824af5a8ff4e0bd5f6a6f546db3523
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-07-04 23:10:45 +0530

    Update React-scheduler version and added functional based

--- src/pages/Basic/functional-based.jsx
+++ src/pages/Basic/functional-based.jsx
@@ -1,5 +1,191 @@
-function Basic() {
-  return <div>Comming Soon</div>;
+import { useEffect, useReducer } from 'react';
+import * as dayjsLocale from 'dayjs/locale/pt-br';
+import * as antdLocale from 'antd/locale/pt_BR';
+
+import { Scheduler, SchedulerData, ViewType, DemoData, wrapperFun } from 'react-big-schedule';
+
+let schedulerData;
+
+const initialState = {
+  showScheduler: false,
+  viewModel: {},
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INITIALIZE':
+      return { showScheduler: true, viewModel: action.payload };
+    case 'UPDATE_SCHEDULER':
+      return { ...state, viewModel: action.payload };
+    case 'REINITIALIZE':
+      return { ...state, showScheduler: false };
+    default:
+      return state;
+  }
 }
 
-export default Basic;
+function Basic() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    schedulerData = new SchedulerData('2022-12-22', ViewType.Week, false, false, {
+      besidesWidth: window.innerWidth <= 1600 ? 100 : 350,
+      dayMaxEvents: 99,
+      weekMaxEvents: 9669,
+      monthMaxEvents: 9669,
+      quarterMaxEvents: 6599,
+      yearMaxEvents: 9956,
+      customMaxEvents: 9965,
+      eventItemPopoverTrigger: 'click',
+      schedulerContentHeight: '100%',
+    });
+    schedulerData.setSchedulerLocale(dayjsLocale);
+    schedulerData.setCalendarPopoverLocale(antdLocale);
+    schedulerData.setResources(DemoData.resources);
+    schedulerData.setEvents(DemoData.events);
+
+    dispatch({ type: 'INITIALIZE', payload: schedulerData });
+
+    return () => dispatch({ type: 'REINITIALIZE' });
+  }, []);
+
+  const prevClick = schedulerData => {
+    schedulerData.prev();
+    schedulerData.setEvents(DemoData.events);
+    dispatch({ type: 'UPDATE_SCHEDULER', payload: schedulerData });
+  };
+
+  const nextClick = schedulerData => {
+    schedulerData.next();
+    schedulerData.setEvents(DemoData.events);
+    dispatch({ type: 'UPDATE_SCHEDULER', payload: schedulerData });
+  };
+
+  const onSelectDate = (schedulerData, date) => {
+    schedulerData.setDate(date);
+    schedulerData.setEvents(DemoData.events);
+    dispatch({ type: 'UPDATE_SCHEDULER', payload: schedulerData });
+  };
+
+  const onViewChange = (schedulerData, view) => {
+    const start = new Date();
+
+    schedulerData.setViewType(view.viewType, view.showAgenda, view.isEventPerspective);
+    schedulerData.setEvents(DemoData.events);
+    dispatch({ type: 'UPDATE_SCHEDULER', payload: schedulerData });
+
+    const secondsBetween = (date1, date2) => {
+      const diff = Math.abs(date1.getTime() - date2.getTime());
+      return diff / 1000;
+    };
+    console.log('Elapsed seconds: ' + secondsBetween(start, new Date()));
+  };
+
+  const ops1 = (schedulerData, event) => {
+    alert(`You just executed ops1 to event: {id: ${event.id}, title: ${event.title}}`);
+  };
+
+  const ops2 = (schedulerData, event) => {
+    alert(`You just executed ops2 to event: {id: ${event.id}, title: ${event.title}}`);
+  };
+
+  const updateEventStart = (schedulerData, event, newStart) => {
+    if (confirm(`Do you want to adjust the start of the event? {eventId: ${event.id}, eventTitle: ${event.title}, newStart: ${newStart}}`)) {
+      schedulerData.updateEventStart(event, newStart);
+    }
+    dispatch({ type: 'UPDATE_SCHEDULER', payload: schedulerData });
+  };
+
+  const updateEventEnd = (schedulerData, event, newEnd) => {
+    if (confirm(`Do you want to adjust the end of the event? {eventId: ${event.id}, eventTitle: ${event.title}, newEnd: ${newEnd}}`)) {
+      schedulerData.updateEventEnd(event, newEnd);
+    }
+    dispatch({ type: 'UPDATE_SCHEDULER', payload: schedulerData });
+  };
+
+  const moveEvent = (schedulerData, event, slotId, slotName, start, end) => {
+    if (
+      confirm(
+        `Do you want to move the event? {eventId: ${event.id}, eventTitle: ${event.title}, newSlotId: ${slotId}, newSlotName: ${slotName}, newStart: ${start}, newEnd: ${end}`
+      )
+    ) {
+      schedulerData.moveEvent(event, slotId, slotName, start, end);
+      dispatch({ type: 'UPDATE_SCHEDULER', payload: schedulerData });
+    }
+  };
+
+  const newEvent = (schedulerData, slotId, slotName, start, end, type, item) => {
+    if (confirm(`Do you want to create a new event? {slotId: ${slotId}, slotName: ${slotName}, start: ${start}, end: ${end}, type: ${type}, item: ${item}}`)) {
+      let newFreshId = 0;
+      schedulerData.events.forEach(item => {
+        if (item.id >= newFreshId) newFreshId = item.id + 1;
+      });
+
+      let newEvent = {
+        id: newFreshId,
+        title: 'New event you just created',
+        start: start,
+        end: end,
+        resourceId: slotId,
+        bgColor: 'purple',
+      };
+      schedulerData.addEvent(newEvent);
+      dispatch({ type: 'UPDATE_SCHEDULER', payload: schedulerData });
+    }
+  };
+
+  const onScrollLeft = (schedulerData, schedulerContent) => {
+    if (schedulerData.ViewTypes === ViewType.Day) {
+      schedulerData.prev();
+      schedulerData.setEvents(DemoData.events);
+      dispatch({ type: 'UPDATE_SCHEDULER', payload: schedulerData });
+      schedulerContent.scrollLeft = 10;
+    }
+  };
+  const onScrollRight = (schedulerData, schedulerContent, maxScrollLeft) => {
+    if (schedulerData.ViewTypes === ViewType.Day) {
+      schedulerData.next();
+      schedulerData.setEvents(DemoData.events);
+      dispatch({ type: 'UPDATE_SCHEDULER', payload: schedulerData });
+      schedulerContent.scrollLeft = maxScrollLeft - 10;
+    }
+  };
+
+  const onScrollTop = () => console.log('onScrollTop');
+
+  const onScrollBottom = () => console.log('onScrollBottom');
+
+  const toggleExpandFunc = (schedulerData, slotId) => {
+    schedulerData.toggleExpandStatus(slotId);
+    dispatch({ type: 'UPDATE_SCHEDULER', payload: schedulerData });
+  };
+
+  return (
+    <>
+      {state.showScheduler && (
+        <Scheduler
+          schedulerData={state.viewModel}
+          prevClick={prevClick}
+          nextClick={nextClick}
+          onSelectDate={onSelectDate}
+          onViewChange={onViewChange}
+          viewEventClick={ops1}
+          viewEventText="Ops 1"
+          viewEvent2Text="Ops 2"
+          viewEvent2Click={ops2}
+          updateEventStart={updateEventStart}
+          updateEventEnd={updateEventEnd}
+          moveEvent={moveEvent}
+          newEvent={newEvent}
+          onScrollLeft={onScrollLeft}
+          onScrollRight={onScrollRight}
+          onScrollTop={onScrollTop}
+          onScrollBottom={onScrollBottom}
+          toggleExpandFunc={toggleExpandFunc}
+        />
+      )}
+    </>
+  );
+}
+
+export default wrapperFun(Basic);
